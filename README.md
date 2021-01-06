5153b9a17d2fcc31dd5d0ce02c0bb134d83378e5
Nashir Janmohamed <nashirbass@gmail.com>
2021-01-06 01:05:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This project is following the [palletsprojects Flask tutorial](https://flask.palletsprojects.com/en/1.1.x/tutorial/)
+
+Run the command `flask init-db` from the top-level dir to initialize/reset the database.
+
+Continue tutorial [from here](https://flask.palletsprojects.com/en/1.1.x/tutorial/tests/)
+
+Read more about [message flashing](https://flask.palletsprojects.com/en/1.1.x/patterns/flashing/)
+
+
+
+
+Think about switching from SQLite to PostGreSQL so that I can have multidimensional array
---
+++ climbr/__init__.py
@@ -0,0 +1,58 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+
+    ## create the Flask instance
+    ### __name__ allows setting up paths
+    ### instance_relative_config tells app that config files are
+    ### relative to the instance folder (which is located outside
+    ### of the flaskr package). the instance folder holds local data
+    ### that shouldn't be commited to VCS, like DB and config secrets
+    app = Flask(__name__, instance_relative_config=True)
+    
+
+    ## set some default configuration that the app will use
+    ### SECRET_KEY is used to keep data safe. should be overridden
+    ### with some random value when deploying, set to 'dev' for
+    ### convenience
+    ### DATABASE is path where SQLite db file will be saved 
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'climbr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return f'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    return app
+
